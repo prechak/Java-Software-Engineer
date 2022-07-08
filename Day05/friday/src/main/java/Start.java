b4bee794761ea4bc511d9d61e6ed1459b051d804
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-08 18:13:03 +0700

    find max

--- Day05/friday/src/main/java/Start.java
+++ Day05/friday/src/main/java/Start.java
@@ -3,12 +3,36 @@ import java.util.Scanner;
 
 class Strat{
     public static void main(String[] z){
-        Cashier c = new Cashier(270.0);
-        double total = c.getTotal();
-        System.out.println(total);
-        // Discount 5% for price at least 100.0
+
+        Total [] total = {  new Total("Liberty", 85),
+                            new Total("Democrat", 72),
+                            new Total("Labour", 89) };
+        if(total.length == 0) {
+        System.out.println("NO DATA");
+        return;
+        }
+        int best = 0;   // let the first element is the best
+        
+        int i = 0;
+        while(i < total.length){
+            //System.out.println( total[i].name + total[i].point );
+            if(total[i].point > total[best].point){
+                best = i;
+            }
+            i++;
+        }
+        System.out.println( total[best].name );
     }
     
+}
+
+class Total{
+    Total(String p, int v){
+        name = p;
+        point = v;
+    }
+    String name;
+    int point;
 }
 
 class Ellipse {
@@ -30,9 +54,8 @@ class Cashier{
     }
     double cash;
     double getTotal(){
-        if( cash > 100) 
+        if( cash < 100) return cash;
         return cash * 0.95;
-        return 0;
     }
 }
 
