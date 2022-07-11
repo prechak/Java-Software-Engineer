f86b44811a37ea4e67093631a5dfea25208187c1
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-11 16:17:12 +0700

    update while loop

--- Day06/monday/code/Start.java
+++ Day06/monday/code/Start.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 class Start{
     public static void main(String[] z){
-        System.out.println("Welcome to Java");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Binary String: ");
+        String line = in.nextLine();
+        char[] a = line.toCharArray();
+        int i = 0;
+        int count = 0;
+        while(i < a.length){
+            System.out.println(a[i]);
+            i++;
+        }
+        
     }
 }
