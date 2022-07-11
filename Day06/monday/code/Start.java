6241e49f90fafe9f3c566b4b08af81aec24ac3e4
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-11 16:43:17 +0700

    add sentinel + 1

--- Day06/monday/code/Start.java
+++ Day06/monday/code/Start.java
@@ -5,6 +5,7 @@ class Start{
         Scanner in = new Scanner(System.in);
         System.out.print("Enter Binary String: ");
         String line = in.nextLine();
+        line = line + '1';
         char[] a = line.toCharArray();
         int count = 0;
         for(int i = 0; i < a.length; i++) {
@@ -16,8 +17,8 @@ class Start{
                 count = 0;          // เจอ 1 แล้ว reset
             }
         }
-        System.out.println(count);
+        //System.out.println(count);  // print last block
         
     }
-}   // 00010010000
+}   // 00010010000 + 1 add Sentinel > 00010010000
     //    3  2    > last block not print should be 4
