ecdd55afa61812071e8233206bff11fe63275987
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-12 11:51:33 +0700

    update Spring 6

---
+++ Day07/sample/code/main/Start.java
@@ -0,0 +1,23 @@
+package main;
+import org.springframework.core.SpringVersion;
+import org.springframework.context.support.GenericApplicationContext;
+
+class Start {
+    public static void main(String[] data) {
+        var version = SpringVersion.getVersion();
+        System.out.println(version);
+        
+        var context = new GenericApplicationContext();
+        context.registerBean(SimpleBean.class);
+        
+        var all = context.getBeanDefinitionNames();
+        for (var s : all) {
+            System.out.println(s);
+        }
+    }
+}
+
+
+class SimpleBean {
+    
+}
