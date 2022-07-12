3e224e12bc3de7351faf4d461dc414eb3b027a9a
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-12 16:52:03 +0700

    update

--- Day07/sample/code/main/Party.java
+++ Day07/sample/code/main/Party.java
@@ -1,14 +1,2 @@
 package main;
 
-class Party {
-    int point;
-    public void setName(String name){  // Writable property
-        
-    }
-    public void setPoint(int point){   // When hava set is Writable property
-        this.point = point;
-    }
-    public boolean isOK(){
-        return point >= 50;     // Readable
-    }
-}
--- Day07/sample/code/main/Start.java
+++ Day07/sample/code/main/Start.java
@@ -35,6 +35,19 @@ class Setup {
     void go(){
         Party p = (Party)context.getBean(Party.class);
         p.setName("Liberty");
-        p.setPoint(72);
+        p.setPoint(42);
+    }
+}
+
+class Party {
+    int point;
+    public void setName(String name){  // Writable property
+        
+    }
+    public void setPoint(int point){   // When hava set is Writable property
+        this.point = point;
+    }
+    public boolean isOK(){
+        return point >= 50;     // Readable
     }
 }
