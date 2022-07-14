6caa8c362215bee42baeef164a4ed79f671d3d60
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-14 17:35:27 +0700

    Update @Qualifier

--- Day09/sample-14/code/main/BranchController.java
+++ Day09/sample-14/code/main/BranchController.java
@@ -1,11 +1,14 @@
 package main;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class BranchController{
+    
+    @Autowired @Qualifier("first") Manager m;
     
     @Autowired Team t;
     
