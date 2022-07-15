f2e8ff044012e76ab5844cb874b1354b18d59404
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-15 17:47:24 +0700

    test change

--- Day10/java-15/code/Second.java
+++ Day10/java-15/code/Second.java
@@ -1,13 +1,21 @@
 
 class Second{
     public static void main(String[] data){
-        Station s;
-        s = new Station();
+        Station first = new Station();
+        first.name = "Bang Sue"; first.area = 50000;
+        Station second = new Station();
+        second.name = "Bang Plad";
         
-        Station t = new Station();
+        first.next = second;    // link
         
-        System.out.println(s);
-        System.out.println(t);
+        Station w = new Station();
+        w.name = "Pin Klao";
+        second.next = w;
+        
+        Station current = first;
+        current.name = "Hello";
+        
+        System.out.println(first.name);
     }
     
 }
@@ -15,4 +23,5 @@ class Second{
 class Station {
     String name;    // default value of String is null
     double area;    // default value of double is 0.0
+    Station next;
 }
