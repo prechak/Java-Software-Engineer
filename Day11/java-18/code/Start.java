dffd4d503d07934f8f7f294ba3df8d0385c429f6
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-19 15:06:44 +0700

    Update

--- Day11/java-18/code/Start.java
+++ Day11/java-18/code/Start.java
@@ -1,20 +1,39 @@
 
 class Start{
     public static void main(String[] z){
+        Tool t = new Tool();
+        Element first = t.build( 8, 5, 4, 2, 1, 9, 4, 7);
+        Element second = t.cut(first);
         
-        int w = Integer.parseInt("123");
-        int y = Integer.valueOf("123");
-        
-        
+        for(Element e = first; e != null; e = e.next)
+            System.out.println();
+        System.out.println();
+        for(Element e = secondl e != null; e = e.next)
+            System.out.println(e.number + " ");
     }
 }
 
 class Tool {
     Element create(int[] a){    // Accept Array
-        return null;
+        Element head = null;
+        Element last = null;
+        for(int i = a.length - 1; i >= 0; i--){
+            last = new Element();
+            last.number = a[i];
+            if(last == null) {last = e; }
+        }
     }
     Element build(int ... a){   // Variadic Method
-        return null;
+        Element first = null;
+        Element last  = null;
+        for(int i = 0; i < a.length; i++){
+            Element e = new Element();
+            e.number = a[i];
+            if(head == null){
+                first
+                
+            }
+        }
     }
 }
 
