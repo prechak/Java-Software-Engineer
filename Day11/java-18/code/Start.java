cab6388afb326d15ed751f7a0a0f229bf94e054a
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-19 16:05:39 +0700

    Data Structure

--- Day11/java-18/code/Start.java
+++ Day11/java-18/code/Start.java
@@ -1,10 +1,43 @@
 
 class Start{
     public static void main(String[] data){
-        Engine e = new Engine();
-        //e.create("");
-        e.play("");
+        Node root;
+        root = new Node();
+        root.name = "David";
+        root.number = 7;
+        
+        root.right = new Node();
+        root.right.name = "Micheal";
+        root.right.number = 10;
+        
+        root.right.right = new Node();
+        root.right.right.name = "Stephen";
+        root.right.right.number = 4;
+        
+        Node x = new Node();
+        x.name = "Frank";
+        x.number = 8;
+        
+        root.left = x;
+        Helper h = new Helper();
+        h.print(root);
+           
     }
+}
+class Helper{
+    void print(Node c){
+        if( c == null) return; // Base Case
+        System.out.println(c.name);
+        print(c.left);
+        print(c.right);
+    }
+}
+
+class Node {
+    String name;    //null
+    int number;     // 0
+    Node left;      // null
+    Node right;     // null
 }
 // In football, there are three result: W, D, L
 // write code to print all possible results
