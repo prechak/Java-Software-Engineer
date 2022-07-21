5f7f5c60a4a5ec03298ed25d2eed61445a2657f8
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-21 16:50:40 +0700

    hash

--- Day13/java-20/code/Start.java
+++ Day13/java-20/code/Start.java
@@ -3,13 +3,8 @@ import java.util.TreeSet;
 
 class Start{
     public static void main(String[] data){
-        TreeSet<String> s = new TreeSet<>();
-        s.add("Latte");
-        s.add("Mocha");
-        s.add("Cappuccino");
-        s.add("Mocha");
-        s.add("Latte");
-        System.out.println(s);
+        int slot = "Latte".hashCode();
+        System.out.println(slot);
 
     }
 }
