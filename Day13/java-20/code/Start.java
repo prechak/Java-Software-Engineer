a8648e687158ddc004f3dcee4989f66ebe76a858
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-21 16:14:01 +0700

    Update

--- Day13/java-20/code/Start.java
+++ Day13/java-20/code/Start.java
@@ -9,6 +9,11 @@ class Start{
         a.add(5);   // convert to instance of Integer
         a.add(7.5); // convert to instance of Double
         a.add("Mocha");
+        a.add("Latte");
+        a.add("Latte");
+        
+        //a.remove("Latte"); //remove first Latte
+        a.remove(3);         //remove from index
         System.out.println(a);
         
         for(Object o : a){
@@ -38,73 +43,3 @@ class Player implements Comparable{
           return this.name.compareTo(p.name);
     }
 }
-
-
-
-
-
-
-/*import java.util.*;
-
-class Start{
-    public static void main(String[] data){
-        Team[] list = { new Team("Bangkok", 1, 5, 4),
-                        new Team("Pattaya", 2, 7, 3),
-                        new Team("Chiang Mai", 4, 3, 1)
-                       };
-        Competitor[] all = {   new Competitor("A", 3,5,4),
-                                new Competitor("B", 4,6,8)
-                            };
-        Arrays.sort(all, (u,v) -> v.gold() - u.gold());
-        
-        for(Competitor c : all) System.out.println(c);
-        
-        
-        
-    }
-}
-
-record Competitor(String name, int gold, int silver, int bronze){}
-
-class SportComparator implements Comparator{
-    @Override
-    public int compare(Object x, Object y){
-        Team u = (Team)x;
-        Team v = (Team)y;
-        return u.gold - v.gold;
-    }
-}
-
-class Team {
-    Team(String name, int gold, int silver, int bronze){
-        this.name = name;
-        this.gold = gold;
-        this.silver = silver;
-        this.bronze = bronze;
-    }
-    String name;
-    int gold, silver, bronze;
-    
-    @Override
-    public String toString(){
-        return name;
-    }
-}
-
-class Worker implements Runnable{
-    @Override public void run(){
-        for(int i = 0; i < 1000; i++){
-            System.out.print('W');
-        }
-    }
-}
-
-
-class Printer extends Thread {
-    @Override
-    public void run(){
-        for(int i = 0; i < 1000; i++){
-            System.out.print('P');
-        }
-    }
-}*/
