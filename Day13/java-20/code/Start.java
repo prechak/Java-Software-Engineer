21c1b824b3b4ad92431009573e41c04b65c6ef43
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-21 16:33:08 +0700

    remove comment

--- Day13/java-20/code/Start.java
+++ Day13/java-20/code/Start.java
@@ -14,33 +14,8 @@ class Start{
         a.add("Mocha");
         a.add("Latte");
         a.add("Latte");
-        
-        //a.remove("Latte"); //remove first Latte
-        //a.remove(3);         //remove from index
-        Object o = a.get(0);
+
         System.out.println(a);
-        
-        int size = a.size();
-        System.out.println(size);
 
-    }
-}
-
-class Player implements Comparable{
-    String name;
-    int number;
-    
-    Player(String name, int number){
-        this.name = name;
-        this.number = number;
-    }
-    
-    @Override
-    public int compareTo(Object o){
-        Player p = (Player)o;
-//        if (this.number < p.number) return -1;
-//        if (this.number > p.number) return +1;
-//        return 0;
-          return this.name.compareTo(p.name);
     }
 }
