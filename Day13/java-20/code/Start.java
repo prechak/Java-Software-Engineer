df38d76e1e5c82a3e8358593abef54a03fa3e949
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-21 16:42:51 +0700

    Birnary Search Tree
    มัน Sort มาเรียบร้อยแล้ว

--- Day13/java-20/code/Start.java
+++ Day13/java-20/code/Start.java
@@ -1,21 +1,15 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
 
 class Start{
     public static void main(String[] data){
-        //ArrayList a = new ArrayList();
-        ArrayList<String> a = new ArrayList<String>();
-        ArrayList<String> b = new ArrayList<>();
-        List<String> c = new ArrayList<>();
-        var d = new ArrayList<String>();
-        
-        a.add("Latte");
-        a.add("Mocha");
-        a.add("Latte");
-        a.add("Latte");
-
-        System.out.println(a);
+        TreeSet s = new TreeSet();
+        s.add("Latte");
+        s.add("Mocha");
+        s.add("Cappuccino");
+        s.add("Mocha");
+        s.add("Latte");
+        System.out.println(s);
 
     }
 }
