9a1f9f0d47b67bc604459f2905b6c8b8d1777879
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-21 16:22:56 +0700

    Update

--- Day13/java-20/code/Start.java
+++ Day13/java-20/code/Start.java
@@ -4,10 +4,13 @@ import java.util.LinkedList;
 
 class Start{
     public static void main(String[] data){
-        ArrayList a = new ArrayList();
+        //ArrayList a = new ArrayList();
+        ArrayList<String> a = new ArrayList<String>();
+        ArrayList<String> b = new ArrayList<>();
+        List<String> c = new ArrayList<>();
+        var d = new ArrayList<String>();
+        
         a.add("Latte");
-        a.add(5);   // convert to instance of Integer
-        a.add(7.5); // convert to instance of Double
         a.add("Mocha");
         a.add("Latte");
         a.add("Latte");
