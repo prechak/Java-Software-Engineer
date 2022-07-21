72ebe115d8dc7e8f6f07b97947809559f6bd02c5
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-21 17:30:28 +0700

    Print column

--- Day13/java-20/code/Start.java
+++ Day13/java-20/code/Start.java
@@ -4,18 +4,31 @@ import java.util.HashMap;
 
 class Start{
     public static void main(String[] data){
-        TreeMap<String, Integer> storage = new TreeMap<>();
-        storage.put("Bang Rak", 10500);
-        storage.put("Bang Plad", 10700);
-        storage.put("Patumwan", 10330);
+        String[][] all = { 
+                                {"Lemonade", "Cola", "Water"},
+                                {"Fried Rice", "Kaprao Rice"},
+                                {"Bua Loy", "Lod Chong", "Sticky Rice Mango" }
+                          };
+        for(int d = 0; d < all[0].length; d++){
+            for(int r = 0; r < all[1].length; r++) {
+                for (int s = 0; s < all[2].length; s++) {
+                    System.out.println(d + " " + r + " " + s);
+                }
+            }
+        }
         
-        storage.put("Bang Rak", 10501);     // มาทีหลังเลือกอันหลัง
-        
+//        TreeMap<String, Integer> storage = new TreeMap<>();
+//        storage.put("Bang Rak", 10500);
+//        storage.put("Bang Plad", 10700);
+//        storage.put("Patumwan", 10330);
+//        
+//        storage.put("Bang Rak", 10501);     // มาทีหลังเลือกอันหลัง
+//        
 //        Integer w = storage.get("Bang Rak");
 //        System.out.println(w);
-
-          for(String s : storage.keySet()) {
-              System.out.println(s);
+//
+//          for(String s : storage.keySet()) {
+//              System.out.println(s);
           }
     }
 }
